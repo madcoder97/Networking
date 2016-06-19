9621d815dbc7b067bad6459e9e0c1289f47cd30c
madcoder97 <shantanukawale@gmail.com>
2016-06-19 23:45:16 +0530

    Create Client.py

---
+++ Client.py
@@ -0,0 +1,27 @@
+from socket import *
+host = 'localhost'#"255.255.255.255"
+port = 12345
+addr = (host,port)
+#msg = "Welcome to the UDP socket chat!"
+s=socket(AF_INET,SOCK_DGRAM) 
+s.bind(addr)
+#s.sendto(msg,addr)
+x=0
+while x==0:
+    try:
+        message, address = s.recvfrom(8192) 
+        if message:
+            print address, "> ", message
+    except:
+        #print "Error"
+        pass
+    if message!="bye":
+       inpu = raw_input()
+       if inpu != False and inpu!="bye":
+          s.sendto(inpu, address)
+       elif inpu=="bye":
+           s.sendto(inpu, address)
+           x=1
+    else:
+       x=1
+s.close()
